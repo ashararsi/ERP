06345f7a53b66a9e0f55335885a9dff4b06201d4
ashar <ashar.azeem@unduit.pk>
2025-03-04 00:41:11 +0500

    s

--- app/Http/Controllers/Admin/PurchaseOrderController.php
+++ app/Http/Controllers/Admin/PurchaseOrderController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Services\PurchaseOrderServices;
 use Illuminate\Http\Request;
 
-class GoodReceiptNoteControllerPurchaseOrderController extends Controller
+class PurchaseOrderController extends Controller
 {
     /**
      * Display a listing of the resource.
