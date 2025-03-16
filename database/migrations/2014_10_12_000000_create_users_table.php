9cb9a1321217ec7fc526d30adc3b0b71dad79c37
Saima <saima@prismatic-technologies.com>
2025-03-16 15:31:08 +0500

    working on seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('role');
+            $table->string('role')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
 
 
 class UserSeeder extends Seeder
@@ -15,18 +16,38 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Add an admin user
-        User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@admin.com',
-            'first_name' => 'admin@admin.com',
-            'last_name' => 'admin@admin.com',
-            'password' => Hash::make('12345678'),
-            'role' => 'admin',
-        ]);
-        $user = User::find(1);
-        $roles = ['Admin'];
-        $user->assignRole($roles);
+//        // Add an admin user
+//        User::create([
+//            'name' => 'Admin User',
+//            'email' => 'admin@admin.com',
+//            'first_name' => 'admin@admin.com',
+//            'last_name' => 'admin@admin.com',
+//            'password' => Hash::make('12345678'),
+//            'role' => 'admin',
+//        ]);
+//        $user = User::find(1);
+//        $roles = ['Admin'];
+//        $user->assignRole($roles);
+
+
+
+        $roles = Role::pluck('name')->toArray(); // Fetch all role names
+
+        foreach ($roles as $roleName) {
+            for ($i = 1; $i <= 5; $i++) { // Create 5 users per role
+                $user = User::create([
+                    'name' => "$roleName User $i",
+                    'email' => strtolower($roleName) . "$i@example.com",
+                    'first_name' => "$roleName User",
+                    'last_name' => "Test $i",
+                    'password' => Hash::make('12345678'),
+                ]);
+
+                $user->assignRole($roleName);
+            }
+        }
+
+
 
     }
 }
