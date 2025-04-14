03dda3f60afbd19c8c8fcb0604daebc006d60e21
ashar <ashar.azeem@unduit.pk>
2025-04-14 12:21:45 +0500

    working on holidays

--- app/Services/HolydaysServices.php
+++ app/Services/HolydaysServices.php
@@ -69,9 +69,9 @@ class HolydaysServices
         $data = Holiday::select('*')->orderBy('id', 'desc');
         return Datatables::of($data)->addIndexColumn()
             ->addColumn('action', function ($row) {
-                $btn = ' <form  method="POST" onsubmit="return confirm(' . "'Are you sure you want to Delete this?'" . ');"  action="' . route("admin.holydays.destroy", $row->id) . '"> ';
-                $btn = $btn . '<a href=" ' . route("admin.holydays.show", $row->id) . '"  class="ml-2"><i class="fas fa-eye"></i></a>';
-                $btn = $btn . ' <a href="' . route("admin.holydays.edit", $row->id) . '" class="ml-2">  <i class="fas fa-edit"></i></a>';
+                $btn = ' <form  method="POST" onsubmit="return confirm(' . "'Are you sure you want to Delete this?'" . ');"  action="' . route("admin.holidays.destroy", $row->id) . '"> ';
+                $btn = $btn . '<a href=" ' . route("admin.holidays.show", $row->id) . '"  class="ml-2"><i class="fas fa-eye"></i></a>';
+                $btn = $btn . ' <a href="' . route("admin.holidays.edit", $row->id) . '" class="ml-2">  <i class="fas fa-edit"></i></a>';
                 $btn = $btn . '<button  type="submit" class="ml-2" ><i class="fas fa-trash"></i></button>';
                 $btn = $btn . method_field('DELETE') . '' . csrf_field();
                 $btn = $btn . ' </form>';
