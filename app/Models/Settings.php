d450e21a95725a43aae452cb81b0e9575fb02cbc
ashar <ashar.azeem@unduit.pk>
2025-04-22 01:20:12 +0500

    wasd

--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Settings extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'type', 'text', 'icons', 'long_description', 'status', 'image'];
+    protected $table = "settings";
 }
