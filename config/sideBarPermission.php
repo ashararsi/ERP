1f79f46a7fd2f25e8d71b89d44940e9750592e0f
Aziz <azizhanan786@gmail.com>
2025-05-07 14:00:43 +0500

    create side bar permissions

---
+++ config/sideBarPermission.php
@@ -0,0 +1,73 @@
+<?php
+
+return [
+
+    'user_management' => [
+        'View Roles List',
+        'View Permissions List',
+        'View Users List',
+        'View Suppliers List',
+        'View Vendors List',
+    ],
+
+    'accounting' => [
+        'View Account Groups',
+        'Create Account Groups',
+        'View Ledgers',
+        'Create Ledgers',
+        'Create Vouchers',
+        'View Entries',
+        'View Chart of Accounts',
+    ],
+
+    'hr' => [
+        'view staff',
+        'create staff',
+        'view holidays',
+        'create holidays',
+        'view leave types',
+        'create leave types',
+        'view leave entitlement',
+        'create leave entitlement',
+        'view leave requests',
+        'create leave requests',
+        'view leaves',
+        'create leaves',
+        'view leave statuses',
+        'create leave statuses',
+        'view loan plans',
+        'create loan plans',
+        'view loans',
+        'create loans',
+        'view Work Shifts',
+        'create Work Shifts',
+        'view work weeks',
+        'create work weeks',
+    ],
+
+    'inventory' => [
+        'View Customers List',
+        'View POS Orders List',
+        'View Purchase Order List',
+        'View GRN List',
+        'View Units',
+        'View Processes List',
+        'View Raw List',
+        'View Product List',
+        'view Categories',
+        'View Formulation',
+        'View Batches',
+        'View Goods-Issuance List',
+        'View Goods-Reciept',
+        'View Packing',
+        'View Inventory',
+    ],
+
+    'location' => [
+        'view companies',
+        'view branches',
+        'view country',
+        'view city',
+    ],
+
+];
